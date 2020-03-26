09f19cba623d8e5194885b4c9240d2d9b2828360
KingDarBoja <stevenbojato04@gmail.com>
2020-03-26 17:15:08 -0500

    fix: apply highlight syntax using DoCheck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, DoCheck } from '@angular/core';
 
 import { Options } from 'easymde';
 import hljs from './highlight.config';
+import { TdTextEditorComponent } from '@covalent/text-editor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
+  @ViewChild(TdTextEditorComponent, { static: false }) public TdTextEditor: TdTextEditorComponent;
   title = 'covalent-editor-fu';
 
   sampleText = 'Hello World!';
 
   covalentEditorOpts: Options = {
     lineWrapping: true,
-    toolbar: [
-      'bold',
-      'italic',
-      'heading',
-      '|',
-      'quote',
-      'unordered-list',
-      'ordered-list',
-      '|',
-      'code',
-      '|',
-      'link',
-      'image',
-      'table',
-      '|',
-      'preview',
-      'side-by-side',
-      'fullscreen',
-      '|',
-      'guide'
-    ],
+    spellChecker: false,
     renderingConfig: {
       codeSyntaxHighlighting: true,
       hljs
-    }
+    },
+    theme: 'default'
   };
+
+  ngDoCheck() {
+    document.querySelectorAll('code').forEach((block) => {
+      // will work once when the next line is uncommented
+      // hljs.highlightBlock(block);
+    });
+  }
 }
--- src/app/highlight.config.ts
+++ src/app/highlight.config.ts
@@ -1,6 +1,6 @@
 import hljs from '@highlightjs/highlight.js/lib/core';
 import javascript from '@highlightjs/highlight.js/lib/languages/javascript';
-import typescript from '@highlightjs/highlight.js/lib/languages/javascript';
+import typescript from '@highlightjs/highlight.js/lib/languages/typescript';
 import python from '@highlightjs/highlight.js/lib/languages/python';
 import r from '@highlightjs/highlight.js/lib/languages/r';
 
@@ -8,5 +8,7 @@ hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('typescript', typescript);
 hljs.registerLanguage('python', python);
 hljs.registerLanguage('r', r);
+
+hljs.debugMode();
 
 export default hljs;
